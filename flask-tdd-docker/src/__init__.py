1af13eadef9fc5a31193514869eeadae776db3ad
Laurentius Valdi Adela <valdilaurentius@gmail.com>
2024-03-23 22:09:49 -0700

    Docker config

--- flask-tdd-docker/src/__init__.py
+++ flask-tdd-docker/src/__init__.py
@@ -1,18 +1,28 @@
-from flask import Flask, jsonify
-from flask_restx import Api, Resource, fields
+import os
+import sys  # Import sys module for printing to stderr
 
+from flask import Flask
+from flask_restx import Api, Resource
+
+# instantiate the app
 app = Flask(__name__)
 
 api = Api(app)
 
-#set config
-app.config.from_object('src.config.DevelopmentConfig')
+# set config
+app_settings = os.getenv('APP_SETTINGS')
+#print(f"APP_SETTINGS value: {app_settings}", file=sys.stderr)
+app.config.from_object(app_settings)
 
+# Debugging: Print app.config to stderr
+#print(app.config, file=sys.stderr)
+
+# Ping endpoint
 class Ping(Resource):
     def get(self):
         return {
             'status': 'success',
             'message': 'pong!'
         }
-    
+
 api.add_resource(Ping, '/ping')
