b9ce9df61fa7d08359bcac058b07e78cb7c43e61
Laurentius Valdi Adela <valdilaurentius@gmail.com>
2024-03-23 23:45:56 -0700

    Postgre Setup

--- flask-tdd-docker/src/__init__.py
+++ flask-tdd-docker/src/__init__.py
@@ -3,6 +3,7 @@ import sys  # Import sys module for printing to stderr
 
 from flask import Flask
 from flask_restx import Api, Resource
+from flask_sqlalchemy import SQLAlchemy
 
 # instantiate the app
 app = Flask(__name__)
@@ -16,6 +17,21 @@ app.config.from_object(app_settings)
 
 # Debugging: Print app.config to stderr
 #print(app.config, file=sys.stderr)
+
+# instantiate the db
+db = SQLAlchemy(app)
+
+# model
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    activate = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
 
 # Ping endpoint
 class Ping(Resource):
