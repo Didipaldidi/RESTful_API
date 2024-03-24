0f8bc4634d436015ffa66225bf68299599d81642
Laurentius Valdi Adela <valdilaurentius@gmail.com>
2024-03-24 12:59:25 -0700

    Convert entrypoint.sh line ending to LF

--- flask-tdd-docker/entrypoint.sh
+++ flask-tdd-docker/entrypoint.sh
@@ -1,9 +1,9 @@
-echo "waiting for postgres..."
+echo "Waiting for postgres..."
 
 while ! nc -z api-db 5432; do
-    sleep 0.1
+  sleep 0.1
 done
 
-echo "PostgeSQL started"
+echo "PostgreSQL started"
 
 python manage.py run -h 0.0.0.0
